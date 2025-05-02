31e3d53e4738d18a2c7db667b8bed6952dfc632e
Azizbek Hafizov <azizbekhafizov678@gmail.com>
2025-05-02 14:46:21 +0500

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Header from './Header/Header.jsx'
+import Product from './product/Product.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Header />
+    <Product />
   </StrictMode>,
 )
--- src/product/Product.jsx
+++ src/product/Product.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './product.css'
+import React, { useEffect, useState } from 'react'
+import './Product.css'
+import axios from 'axios';
 
-export default function product() {
+export default function Product() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div className='product__container'>
+      <input type="text" placeholder='Searching...' />
 
+
+      <div className="product__wrapper">
+        {products.map((product) => (
+         <div className="product" key={product.id}>
+         <img src={product.image} alt={product.title} />
+         <h2>{product.description}</h2>
+       </div>
+        ))}
+      </div>
       
     </div>
   )
